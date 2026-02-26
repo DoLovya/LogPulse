9c7f2333b645a6f2051792e7c3b34f7e041dcedf
DoLovya <3063676257@qq.com>
2026-02-26 19:24:27 +0800

    feat: 添加阿里巴巴普惠体并设为应用默认字体
    - 新增阿里巴巴普惠体字体文件（OTF 和 WOFF 格式）
    - 在资源文件中注册新增字体
    - 修改 main.cpp，在应用启动时加载并设置该字体为全局默认字体
    - 移除 style.qss 中硬编码的字体族设置，避免与程序化设置的字体冲突

--- main.cpp
+++ main.cpp
@@ -1,23 +1,37 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 
 #include <QApplication>
 #include <QFile>
+#include <QFontDatabase>
+#include <QDebug>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv);
+	QApplication a(argc, argv);
 
-    QCoreApplication::setOrganizationName("LogPulse");
-    QCoreApplication::setApplicationName("LogPulse");
+	QCoreApplication::setOrganizationName("LogPulse");
+	QCoreApplication::setApplicationName("LogPulse");
 
-    QFile styleFile(":/styles/style.qss");
-    if (styleFile.open(QFile::ReadOnly))
-    {
-        QString style = QLatin1String(styleFile.readAll());
-        a.setStyleSheet(style);
-    }
+	// Load Font
+	int fontId = QFontDatabase::addApplicationFont(":/fonts/AlibabaPuHuiTi375SemiBold-Regular.otf");
+	if (fontId != -1) {
+		QStringList fontFamilies = QFontDatabase::applicationFontFamilies(fontId);
+		if (!fontFamilies.isEmpty()) {
+			QString fontFamily = fontFamilies.first();
+			QFont font(fontFamily);
+			font.setPointSizeF(10);
+			a.setFont(font);
+		}
+	}
 
-    MainWindow w;
-    w.show();
-    return a.exec();
+	QFile styleFile(":/styles/style.qss");
+	if (styleFile.open(QFile::ReadOnly))
+	{
+		QString style = QLatin1String(styleFile.readAll());
+		a.setStyleSheet(style);
+	}
+
+	MainWindow w;
+	w.show();
+	return a.exec();
 }
